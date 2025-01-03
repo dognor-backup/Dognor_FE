f66aefd8c3ac43e94beeaa6461ba1e755750bf71
Jungyu Park <parkjungyu09@gmail.com>
2025-01-03 15:19:36 +0900

    style: remove clicked button outline, remove fixed width

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import React from "react";
 import { css, Global, ThemeProvider } from "@emotion/react";
 import { theme } from "./shared/styles/theme";
 import { GlobalStyles } from "./shared/styles/global";
-import styled from "@emotion/styled";
-import { Button } from "./shared/components/buttons/Button";
-import { IconBtn } from "./shared/components/buttons/IconBtn";
-import Plus from "./assets/icons/Plus.svg?react";
+
 
 function App() {
   return (
