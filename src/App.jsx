fb0ea1e7970cfe583fa7bce8347aeadebce2cb45
Jungyu Park <parkjungyu09@gmail.com>
2025-01-03 15:44:52 +0900

    style: button hover outline none

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,11 @@ import { css, Global, ThemeProvider } from "@emotion/react";
 import { theme } from "./shared/styles/theme";
 import { GlobalStyles } from "./shared/styles/global";
 
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={GlobalStyles} />
-    
+      
     </ThemeProvider>
   );
 }
