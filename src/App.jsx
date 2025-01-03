0b055acad2db9bdaf06e98566035f45284c4fd7d
Jungyu Park <parkjungyu09@gmail.com>
2025-01-03 21:10:48 +0900

    chore: assets/images

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { css, Global, ThemeProvider } from "@emotion/react";
 import { theme } from "./shared/styles/theme";
 import { GlobalStyles } from "./shared/styles/global";
-import { LogoutBtn } from "./shared/components/nav/LogoutBtn";
 import Nav from "./shared/components/nav/Nav";
 
 function App() {
