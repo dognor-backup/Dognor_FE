d30abe58f10ffd0e7d52b60e96353689f5d85c26
Jungyu Park <parkjungyu09@gmail.com>
2025-01-03 19:31:15 +0900

    style: shared button components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { css, Global, ThemeProvider } from "@emotion/react";
 import { theme } from "./shared/styles/theme";
 import { GlobalStyles } from "./shared/styles/global";
+import { Button } from "./shared/components/buttons/Button";
 
 function App() {
   return (
