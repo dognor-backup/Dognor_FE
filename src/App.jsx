9f65ce7d0ba1be9b7cc3286a904771ee9bf1a143
leekjoo <leekjoo1008@gmail.com>
2025-01-03 19:49:22 +0900

    style: checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 /** @jsxImportSource @emotion/react */
-import React from "react";
+import React, { useState } from "react";
 import { css, Global, ThemeProvider } from "@emotion/react";
 import { theme } from "./shared/styles/theme";
 import { GlobalStyles } from "./shared/styles/global";
+import Checkbox from "./shared/components/checkbox/Checkbox";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={GlobalStyles} />
-      
+      <Checkbox />
     </ThemeProvider>
   );
 }
---
+++ src/shared/components/checkbox/Checkbox.jsx
@@ -0,0 +1,54 @@
+import styled from "@emotion/styled";
+import { useState } from "react";
+
+const Checkbox = () => {
+  const [isChecked, setChecked] = useState(false);
+  return (
+    <div>
+      <InputChk
+        type="checkbox"
+        name=""
+        id="checkbox"
+        onChange={() => setChecked((prev) => !prev)}
+        checked={isChecked}
+      />
+      {/* size={"small"} 작은 사이즈 체크박스 */}
+      <LabelChk htmlFor="checkbox" checked={isChecked}>
+        Accept terms and conditions
+      </LabelChk>
+    </div>
+  );
+};
+export default Checkbox;
+
+const InputChk = styled.input`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+  appearance: none;
+`;
+const LabelChk = styled.label(({ theme, checked, size }) => ({
+  fontWeight: size === "small" ? 400 : 700,
+  display: "flex",
+  alignItems: "center",
+  cursor: "pointer",
+  color: theme.colors.neutrals_01,
+  "&::before": {
+    content: '""',
+    display: "inline-block",
+    marginRight: "8px",
+    width: size === "small" ? "12px" : "24px",
+    height: size === "small" ? "12px" : "24px",
+    border: `1px solid ${theme.colors.neutrals_01}`,
+    backgroundColor: checked
+      ? theme.colors.neutrals_08
+      : theme.colors.neutrals_01,
+    borderRadius: "4px",
+    backgroundImage: "url('/src/assets/icons/Check_W.svg')",
+    backgroundSize: "15px",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+  },
+}));
