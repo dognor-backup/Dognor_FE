4454f6ee2bc2a46c544d4ba9b10db17aec3c1776
Jungyu Park <parkjungyu09@gmail.com>
2025-01-04 16:31:22 +0900

    style: shared Nav component

--- src/shared/components/nav/TopMenuContainer.jsx
+++ src/shared/components/nav/TopMenuContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { TopMenuBtn } from "./TopMenuBtn";
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
 
 export default function TopMenuContainer() {
   return (
@@ -11,6 +12,7 @@ export default function TopMenuContainer() {
       <TopMenuBtn>커뮤니티</TopMenuBtn>
       <TopMenuBtn>캠페인</TopMenuBtn>
       <TopMenuBtn>About Us</TopMenuBtn>
+      <TopMenuLink>링크</TopMenuLink>
     </TopMenuLayout>
   );
 }
@@ -21,4 +23,22 @@ const TopMenuLayout = styled.div`
   align-content: center;
   padding: 8px 36px;
   gap: 24px;
+`;
+
+const TopMenuLink = styled(Link)`
+  text-decoration: none;
+  min-width: 95px;
+  padding: 8px;
+  gap: 8px;
+  font-size: 18px;
+  line-height: 24px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.blue_dark_400};
+  background-color: transparent;
+  &:hover {
+    background-color: lavender;
+  }
+  &:focus {
+    background-color: black;
+  }
 `;
