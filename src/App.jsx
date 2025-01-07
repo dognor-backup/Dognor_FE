4ee9c8a0c945a1f6559af59c0cbf21dd04cc9d97
leekjoo <leekjoo1008@gmail.com>
2025-01-07 15:54:13 +0900

    style: footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Footer from "./shared/components/footer/Footer";
 import LogoNav from "./shared/components/nav/LogoNav";
 import Nav from "./shared/components/nav/Nav";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={GlobalStyles} />
-    </ThemeProvider>
+    <>
+      <Nav />
+      <Footer />
+    </>
   );
 }
 
