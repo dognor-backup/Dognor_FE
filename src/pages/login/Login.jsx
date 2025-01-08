258b045f3f55d23d3a630cdd52c3e5f2cecf9c1f
Jungyu Park <parkjungyu09@gmail.com>
2025-01-08 17:02:56 +0900

    feat: validation for login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,32 +2,61 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/shared/components/buttons/Button";
 import Checkbox from "@/shared/components/checkbox/Checkbox";
+import { validateId, validatePassword } from "@/shared/utils/validation";
 import styled from "@emotion/styled";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
+  const idRef = useRef(null);
+  const pwRef = useRef(null);
+  const [isIdValid, setIsIdValid] = useState("true");
+  const [isPasswordValid, setIsPasswordValid] = useState("true");
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    const id = idRef.current.value;
+    const password = pwRef.current.value;
+
+    if (!validateId(id)) setIsIdValid("false");
+    if (!validatePassword(password)) return setIsPasswordValid("false");
+  };
   return (
     <LoginLayout>
+      <TextWrapper>
+        <LoginText>로그인</LoginText>
+        <WelcomeText>
+          안녕하세요 :)
+          <br /> 반려견 헌혈문화를 함께 만들어요
+        </WelcomeText>
+      </TextWrapper>
       <LoginContainer>
-        <TextWrapper>
-          <LoginText>로그인</LoginText>
-          <WelcomeText>
-            안녕하세요 :)
-            <br /> 반려견 헌혈문화를 함께 만들어요
-          </WelcomeText>
-        </TextWrapper>
-        <InputContainer>
+        <InputContainer onSubmit={handleLoginSubmit} id="loginForm">
           <InputWrapper>
             <Label htmlFor="id">아이디</Label>
-            <Input type="text" id="id" placeholder="아이디를 입력해주세요" />
+            <Input
+              ref={idRef}
+              type="text"
+              name="id"
+              placeholder="아이디를 입력해주세요"
+              required
+            />
+            <InputDescription isVisible={isIdValid}>
+              다시 확인해주세요
+            </InputDescription>
           </InputWrapper>
           <InputWrapper>
             <Label htmlFor="password">비밀번호</Label>
             <Input
-              type="text"
-              password="password"
+              ref={pwRef}
+              type="password"
+              name="password"
               placeholder="비밀번호를 입력해주세요"
+              required
             />
+            <InputDescription isVisible={isPasswordValid}>
+              다시 확인해주세요
+            </InputDescription>
           </InputWrapper>
           <LoginOptionsContainer>
             <Checkbox
@@ -41,7 +70,9 @@ export default function Login() {
           </LoginOptionsContainer>
         </InputContainer>
         <AuthButtonContainer>
-          <Button>로그인</Button>
+          <Button type="submit" form="loginForm">
+            로그인
+          </Button>
           <Button state="outline">회원가입</Button>
         </AuthButtonContainer>
       </LoginContainer>
@@ -52,17 +83,20 @@ export default function Login() {
 const LoginLayout = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
   width: 100%;
-  /* padding-top: 100px; */
+  flex-direction: column;
+  /* height: 100%; */
+  margin-top: 130px;
 `;
 
 const LoginContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 320px;
-  padding: 100px 0;
+  margin: 0 auto;
+  /* padding: 100px 0; */
   gap: 32px;
+  padding-bottom: 100px;
 `;
 
 const TextWrapper = styled.div`
@@ -71,6 +105,8 @@ const TextWrapper = styled.div`
   gap: 8px;
   justify-content: center;
   align-items: center;
+  padding: 100px 0 32px 0;
+  /* margin: 130px 0 32px 0; */
 `;
 
 const LoginText = styled.p`
@@ -89,7 +125,7 @@ const WelcomeText = styled.p`
   color: ${({ theme }) => theme.colors.neutrals_00};
 `;
 
-const InputContainer = styled.div`
+const InputContainer = styled.form`
   display: flex;
   flex-direction: column;
   gap: 18px;
@@ -112,6 +148,7 @@ const FindAccountLink = styled(Link)`
   padding: 4px;
   gap: 8px;
   font-size: 14px;
+  line-height: 16px;
   font-weight: 400;
   color: ${({ theme }) => theme.colors.neutrals_01};
 `;
@@ -120,4 +157,12 @@ const AuthButtonContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 8px;
+`;
+
+const InputDescription = styled.p`
+  display: ${({ isVisible }) => (isVisible === "true" ? "none" : "block")};
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 24px;
+  color: ${({ theme }) => theme.colors.point_orange_normal_300};
 `;
--- src/shared/styles/global.js
+++ src/shared/styles/global.js
@@ -150,5 +150,6 @@ export const GlobalStyles = css`
 
   #root {
     width: 100%;
+    height: 100vh;
   }
 `;
