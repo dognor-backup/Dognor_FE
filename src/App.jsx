07210ebca607082b7212155f291ef6b90bf47f60
Jungyu Park <parkjungyu09@gmail.com>
2025-01-10 21:07:51 +0900

    style: input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { Global, ThemeProvider } from "@emotion/react";
-import { theme } from "./shared/styles/theme";
-import { GlobalStyles } from "./shared/styles/global";
+import Nav from "./shared/components/nav/Nav";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={GlobalStyles} />
-    </ThemeProvider>
+    <>
+      <Nav />
+    </>
   );
 }
 
