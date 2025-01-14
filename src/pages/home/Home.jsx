342edd2ea01bb38b588aab3edd005775e297a097
Jungyu Park <parkjungyu09@gmail.com>
2025-01-14 23:40:35 +0900

    feat: PostCard Component' '

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,33 @@
+import PostCard from "@/shared/components/postcard/PostCard";
+import { useState } from "react";
+
 export default function Home() {
-  return <>home</>;
+  const [likes, setLikes] = useState(0);
+  const imageURL = "/src/assets/images/dog";
+  return (
+    <>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <PostCard
+          imageUrl={imageURL}
+          text="강아지가 헌혈을 했어요! 강아지가 헌혈을 했어요! 강아지가 헌혈을 했어요! 강아지가 헌혈을 했어요! 강아지가 헌혈을 했어요! 강아지가 헌혈을 했어요! "
+          dogName="바둑이"
+          likes={likes}
+          hashtags={["헌혈", "반려견"]}
+          isAuthor={true}
+          isAdmin={false}
+          handleEdit={() => console.log("수정 클릭!")}
+          handleDelete={() => console.log("삭제 클릭!")}
+          onLike={() => setLikes((prev) => prev + 1)}
+        />
+      </div>
+    </>
+  );
 }
--- src/shared/components/postcard/PostCard.jsx
+++ src/shared/components/postcard/PostCard.jsx
@@ -1,9 +1,127 @@
+import styled from "@emotion/styled";
+import HeartFilled from "../../../assets/icons/red/heart_filled_r.svg?react";
+import DotsVertical from "../../../assets/icons/white/dots_vertical_w.svg?react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import ActionSelect from "./ActionSelect";
 
-export default function PostCard() {
-    return (
-        <div>
-            
-        </div>
-    );
+export default function PostCard({
+  imageUrl,
+  text,
+  dogName,
+  likes,
+  isAuthor,
+  handleEdit,
+  handleDelete,
+  onLike,
+}) {
+  const imageurl = "/src/assets/images/dog.jpeg";
+  const profile = "/src/assets/images/profile.jpeg";
+
+
+  return (
+    <PostCardLayout>
+      <ImageWrapper>
+        <CardImage src={imageurl} />
+       {isAuthor?  <ActionButtonWrapper>
+          <ActionSelect handleEdit={handleEdit} handleDelete={handleDelete} />
+        </ActionButtonWrapper> : "" }
+      </ImageWrapper>
+      <TextWrapper>{text}</TextWrapper>
+      <InfoContainer>
+        <ProfileWrapper>
+          <ProfileImage src={profile} />
+          {dogName}
+        </ProfileWrapper>
+        <LikesWrapper>
+          {likes} <HeartFilled onClick={onLike} />
+        </LikesWrapper>
+      </InfoContainer>
+    </PostCardLayout>
+  );
 }
 
+const PostCardLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: ${({ theme }) => `1px solid ${theme.colors.neutrals_05}`};
+  background-color: ${({ theme }) => {
+    theme.colors.neutrals_08;
+  }};
+  border-radius: 16px;
+  min-width: 234px;
+  max-width: 364px;
+  padding: 12px;
+  gap: 12px;
+`;
+
+const ImageWrapper = styled.div`
+  position: relative;
+  max-height: 280px;
+  border-radius: 10px;
+`;
+
+const CardImage = styled.img`
+  width: 100%;
+  max-height: 280px;
+  object-fit: cover;
+  border-radius: 10px;
+`;
+
+const TextWrapper = styled.div`
+  height: 48px;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 24px;
+  color: ${({ theme }) => {
+    theme.colors.neutrals_00;
+  }};
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProfileWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-weight: 700;
+  color: ${({ theme }) => {
+    theme.colors.neutrals_00;
+  }};
+`;
+
+const ProfileImage = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 100%;
+`;
+
+const LikesWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  cursor: pointer;
+`;
+
+const ActionButtonWrapper = styled.button`
+  position: absolute;
+  background-color: transparent;
+  top: 16px;
+  right: 8px;
+  padding: 0;
+`;
