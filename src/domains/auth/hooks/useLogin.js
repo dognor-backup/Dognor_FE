3b4096099fcd72530d4953645e24df8549820745
Jungyu Park <parkjungyu09@gmail.com>
2025-01-14 14:56:29 +0900

    feat: Dynamic Button Updates in TopNavHeader Based on User Login and Logout Status

--- src/domains/auth/hooks/useLogin.js
+++ src/domains/auth/hooks/useLogin.js
@@ -6,14 +6,12 @@ import useUserStore from "../store/useUserStore";
 export const useLogin = (setValidationError) => {
   const navigate = useNavigate();
   const { setUser } = useUserStore();
-  const { user } = useUserStore;
   return useMutation({
     mutationFn: login,
     onSuccess: ({ success, data }) => {
       if (success) {
         const { userSeq, userId, userRole, name, accessToken } = data.data;
         setUser({ userSeq, userId, userRole, name, accessToken });
-        console.log("Updated user:", useUserStore.getState().user);
         localStorage.setItem("accessToken", accessToken);
         navigate("/home", { replace: true });
       } else {
