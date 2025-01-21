4b57ba6a13f7a0c44d80a1a9a1eb6fdeab417223
Jungyu Park <parkjungyu09@gmail.com>
2025-01-21 20:19:16 +0900

    feat: indexedDB 유저 정보 추가,삭제

--- src/domains/auth/utils/indexedDB.js
+++ src/domains/auth/utils/indexedDB.js
@@ -21,3 +21,36 @@ export const openDatabase = () => {
     });
   };
   
+
+  export const saveUserToDB = async (user) => {
+    const db = await openDatabase();
+    const transaction = db.transaction("AuthStore", "readwrite");
+    const store = transaction.objectStore("AuthStore");
+  
+    const userData = {
+      id: "currentUser",
+      ...user,
+    };
+  
+    return new Promise((resolve, reject) => {
+      const request = store.put(userData);
+  
+      request.onsuccess = () => resolve("유저 정보 저장 성공!");
+      request.onerror = (event) => reject("유저 정보 저장 실패: " + event.target.error);
+    });
+  };
+  
+
+  export const clearUserFromDB = async () => {
+    const db = await openDatabase();
+    const transaction = db.transaction("AuthStore", "readwrite");
+    const store = transaction.objectStore("AuthStore");
+  
+    return new Promise((resolve, reject) => {
+      const request = store.delete("currentUser");
+  
+      request.onsuccess = () => resolve("유저 정보 삭제 성공!");
+      request.onerror = (event) => reject("유저 정보 삭제 실패: " + event.target.error);
+    });
+  };
+  
