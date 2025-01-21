8666ba3215900db3f346195f489e255c659864fb
Jungyu Park <parkjungyu09@gmail.com>
2025-01-21 20:20:38 +0900

    feat: useLogin 유저정보 indexedDB 저장

--- src/domains/auth/hooks/useLogin.js
+++ src/domains/auth/hooks/useLogin.js
@@ -2,17 +2,22 @@ import { useMutation } from "@tanstack/react-query";
 import { login } from "@/domains/auth/api/auth";
 import { useNavigate } from "react-router-dom";
 import useUserStore from "../store/useUserStore";
+import { saveUserToDB } from "@/domains/auth/utils/indexedDB";
 
 export const useLogin = (setValidationError) => {
   const navigate = useNavigate();
   const { setUser } = useUserStore();
+
   return useMutation({
     mutationFn: login,
-    onSuccess: ({ success, data }) => {
+    onSuccess: async ({ success, data }) => {
       if (success) {
         const { userSeq, userId, userRole, name, accessToken } = data.data;
         setUser({ userSeq, userId, userRole, name, accessToken });
-        localStorage.setItem("accessToken", accessToken);
+
+        await saveUserToDB({ userSeq, userId, userRole, name, accessToken });
+        console.log("유저 정보가 IndexedDB에 저장되었습니다.");
+
         navigate("/home", { replace: true });
       } else {
         setValidationError("error");
