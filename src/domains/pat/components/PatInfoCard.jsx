4ca1fd6477d3cc1de8162f0be1ca3cbf2ba76408
Jungyu Park <parkjungyu09@gmail.com>
2025-02-12 15:27:04 +0900

    feat: 데이터 유/무 시 + 버튼을 통한 모달 컴포넌트 공유

--- src/domains/pat/components/PatInfoCard.jsx
+++ src/domains/pat/components/PatInfoCard.jsx
@@ -33,42 +33,46 @@ export default function PatInfoCard() {
             : "아직 함께 하는 반려견이 없습니다"}
         </PatInfoHeaderSubText>
       </PatInfoHeaderTextContainer>
-      {data?.data?.data.length >= 1 ? (
-        <PatInfoCardLayout>
-          <IconBtnContainer>
-            <IconBtn>
-              <PlusIcon />
-            </IconBtn>
-          </IconBtnContainer>
-          {data.data.data.map((pat, index) => (
-            <PatInfoCardWrapper key={index}>
-              <PatProfileImg src={pat.imgUrl} />
-              <PatInfoWrapper>
-                <PatNameText>{pat.name}</PatNameText>
-                <PatInfoContainer>
-                  <PatInfoText>나이: {calculateAge(pat.birthday)}</PatInfoText>
-                  <PatInfoText>분류: {pat.breedName}</PatInfoText>
-                  <PatInfoText>몸무게: {pat.weight} Kg</PatInfoText>
-                  <PatInfoText>혈액형: {pat.bloodTypeName}</PatInfoText>
-                  {pat.isDonor === 1 ? (
-                    <PatInfoHighlightedText>헌혈 가능</PatInfoHighlightedText>
-                  ) : (
-                    <PatInfoBoldText>헌혈 불가능</PatInfoBoldText>
-                  )}
-                </PatInfoContainer>
-              </PatInfoWrapper>
-            </PatInfoCardWrapper>
-          ))}
-        </PatInfoCardLayout>
-      ) : (
-        <>
-          <AddDogBtn onClick={() => openModal("addDogInfo")}>
-            <BlackPlus />
-            강아지 정보를 추가해주세요:)
-          </AddDogBtn>
-          <AddPatInfoCard />
-        </>
-      )}
+      <PatInfoContentLayout>
+        <IconBtnContainer>
+          <IconBtn onClick={() => openModal("addDogInfo")}>
+            <PlusIcon />
+          </IconBtn>
+        </IconBtnContainer>
+        <AddPatInfoCard />
+        {data?.data?.data.length >= 1 ? (
+          <PatInfoCardLayout>
+            {data.data.data.map((pat, index) => (
+              <PatInfoCardWrapper key={index}>
+                <PatProfileImg src={pat.imgUrl} />
+                <PatInfoWrapper>
+                  <PatNameText>{pat.name}</PatNameText>
+                  <PatInfoContainer>
+                    <PatInfoText>
+                      나이: {calculateAge(pat.birthday)}
+                    </PatInfoText>
+                    <PatInfoText>분류: {pat.breedName}</PatInfoText>
+                    <PatInfoText>몸무게: {pat.weight} Kg</PatInfoText>
+                    <PatInfoText>혈액형: {pat.bloodTypeName}</PatInfoText>
+                    {pat.isDonor === 1 ? (
+                      <PatInfoHighlightedText>헌혈 가능</PatInfoHighlightedText>
+                    ) : (
+                      <PatInfoBoldText>헌혈 불가능</PatInfoBoldText>
+                    )}
+                  </PatInfoContainer>
+                </PatInfoWrapper>
+              </PatInfoCardWrapper>
+            ))}
+          </PatInfoCardLayout>
+        ) : (
+          <>
+            <AddDogBtn onClick={() => openModal("addDogInfo")}>
+              <BlackPlus />
+              강아지 정보를 추가해주세요:)
+            </AddDogBtn>
+          </>
+        )}
+      </PatInfoContentLayout>
     </PatInfoLayout>
   );
 }
@@ -111,7 +115,6 @@ const PatInfoCardLayout = styled.div`
   gap: 21px;
   justify-content: center;
   align-items: center;
-  margin-top: 56px;
 `;
 
 const PatInfoCardWrapper = styled.div`
@@ -170,9 +173,10 @@ const PatInfoBoldText = styled.p`
 `;
 
 const IconBtnContainer = styled.div`
-  position: absolute;
-  top: -56px;
-  right: -133.5px;
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  margin-bottom: 16px;
 `;
 
 const AddDogBtn = styled.button`
@@ -187,4 +191,12 @@ const AddDogBtn = styled.button`
   gap: 16px;
   box-shadow: 4px 4px 16px 0px rgba(0, 0, 0, 0.25);
   background-color: ${({ theme }) => theme.colors.neutrals_08};
+`;
+
+const PatInfoContentLayout = styled.div`
+  width: 1008px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
